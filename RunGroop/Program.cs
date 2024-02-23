4bde990fab5bedd7627b68e02a8f84fc909f9f04
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-02-23 00:01:23 -0300

    Added EF and DB Context

--- RunGroop/Program.cs
+++ RunGroop/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RunGroop.Data;
+
 namespace RunGroop
 {
 	public class Program
@@ -8,6 +11,12 @@ namespace RunGroop
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
+
+			// Add the database context to the container.
+			builder.Services.AddDbContext<AppDbContext>(options =>
+			{
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
 
 			var app = builder.Build();
 
